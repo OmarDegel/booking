9cef953014ac0d8bab21624ec20f0dc8e3e56742
Omar Degel <osamy8088@gmail.com>
2026-02-27 16:46:20 +0200

    edit api

--- src/server/axios.global.ts
+++ src/server/axios.global.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../store";
 import { logout } from "../store/user/userSlice";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
+axios.defaults.baseURL = "https://admin.sharm-dream.com/api/";
 
 axios.interceptors.response.use(
   (response) => response,
--- src/server/unautorizrd.globla.ts
+++ src/server/unautorizrd.globla.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../store";
 import { logout } from "../store/user/userSlice";
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://admin.sharm-dream.com/api/",
   headers: {
     Accept: "application/json",
   },
